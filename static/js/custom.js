bd691edfa8ec5f4baf2e0f9d1b539450d9e4842f
Ubuntu <ubuntu@ip-172-31-89-208.ec2.internal>
2019-08-01 16:56:04 +0000

    custom js file added and basic function to show search and navbar added

---
+++ static/js/custom.js
@@ -0,0 +1,20 @@
+//Function to display my search form on click
+
+$(document).ready(function() {
+    $('#search_icon').click(function() {
+        $('#search_form').toggle();
+    });
+});
+
+
+// Materialize Customiztions
+$(".button-collapse").sideNav();
+
+$(document).ready(function() {
+    $('.tooltipped').tooltip();
+});
+
+$(document).ready(function() {
+    $('#search_cities').collapsible();
+});
+
