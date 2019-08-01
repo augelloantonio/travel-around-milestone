063477d8c329ba9447c82aca64b1e02ecdc2e95d
Ubuntu <ubuntu@ip-172-31-89-208.ec2.internal>
2019-08-01 18:03:58 +0000

    def get_city, insert_city, add_city added

---
+++ app.py
@@ -0,0 +1,39 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
+
+#Inizialize Flask
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'travel_around'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+mongo = PyMongo(app)
+
+#Set as homepage my index.html
+@app.route('/')
+def index():
+    return render_template("index.html")
+    
+@app.route('/add_city')
+def add_city():
+    return render_template('addcity.html',
+    categories=mongo.db.categories.find())
+    
+@app.route('/get_cities')
+def get_cities():
+    return render_template("index.html", cities=mongo.db.cities.find())
+    
+@app.route('/insert_city', methods=['POST'])
+def insert_city():
+    cities = mongo.db.cities
+    cities.insert_one(request.form.to_dict())
+    return redirect(url_for('index'))
+
+
+#Permitt the server to run the web app
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get("PORT")),
+            debug=True)
+            
