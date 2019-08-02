c8c773671406be909773f29600f941a704789490
Ubuntu <ubuntu@ip-172-31-89-208.ec2.internal>
2019-08-02 15:09:37 +0000

    added import lines

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import os
 import json
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 #~~~~~~~~~~~~~~~~~~Testing if the json file is imported~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
@@ -9,5 +12,4 @@ with open('data/countries.json') as json_file:
         print (name['name'])
         
 
-    
 
