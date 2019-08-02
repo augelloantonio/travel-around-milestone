53b02782995b0e7cad5e634af8fe9876aca2b93e
Ubuntu <ubuntu@ip-172-31-89-208.ec2.internal>
2019-08-02 15:02:24 +0000

    edit and update function now working

--- app.py
+++ app.py
@@ -15,10 +15,12 @@ mongo = PyMongo(app)
 #Set as homepage my index.html
 @app.route('/')
 def index():
-    return render_template("index.html", cities=mongo.db.cities.find(),
+    with open('data/countries.json') as json_file:
+        json_file = json.loads(json_file.read())
+    return render_template("index.html", cities=mongo.db.cities.find(), country=json_file,
     city_category=mongo.db.city_category.find())
     
-
+# Connect to the database file and add a new city 
 @app.route('/add_city')
 def add_city():
     country=[]
@@ -27,14 +29,14 @@ def add_city():
         return render_template('addcity.html', country=json_file,
         city=mongo.db.cieties.find())
     
-    
+# Add a new city and then redirect to index
 @app.route('/insert_city', methods=['POST'])
 def insert_city():
     cities = mongo.db.cities
     cities.insert_one(request.form.to_dict())
     return redirect(url_for('index'))
     
-    
+# Get the city data from the city id
 @app.route('/edit_city/<city_id>')
 def edit_city(city_id):
     the_city =  mongo.db.cities.find_one({"_id": ObjectId(city_id)})
@@ -52,7 +54,7 @@ def update_city(city_id):
     {
         'city_name':request.form.get('city_name'),
         'city_country':request.form.get('city_country'),
-        'city_population': request.form.getlist('city_population'),
+        'city_population': request.form.get('city_population'),
         'city_description': request.form.get('city_description'),
         'city_must_see': request.form.getlist('city_must_see'),
         'city_category': request.form.getlist('city_category'),
@@ -60,6 +62,7 @@ def update_city(city_id):
         'city_image':request.form.get('city_image')
     })
     return redirect(url_for('index'))
+
 
 # Delete city - to add an if statement
 @app.route('/delete_city/<city_id>')
