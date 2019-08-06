3691eca2e5f8bac4a8622f794107d24157efcd03
Ubuntu <ubuntu@ip-172-31-89-208.ec2.internal>
2019-08-06 17:29:48 +0000

    added function for signup - full working - and login in progress

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import os
+import app
 import json
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
 from time import ctime
+import unittest
 
 #Inizialize Flask
 app = Flask(__name__)
@@ -36,3 +38,19 @@ print('')
 
 #~~~~~~~~~~~~~~~~~~Testing the links ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
+class testing_links(unittest.TestCase):
+    
+    def setUp(self):
+        self.mongo = app.test_client()
+
+    def test_home(self):
+        page = self.mongo.get('/')
+        self.assertEqual(page.status_code, 202)
+
+    def test_add_city(self):
+        response = self.mongo.get('/add_city')
+        self.assertEqual(response.status_code, 200)
+        
+        
+if __name__ == '__main__':
+    unittest.main()
