0a4fb399a32858ad3511a2c4707acd3a7a2fc37d
Ubuntu <ubuntu@ip-172-31-89-208.ec2.internal>
2019-08-06 09:52:24 +0000

    tested mongodb database import and actual time settings

--- test.py
+++ test.py
@@ -3,13 +3,36 @@ import json
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
+from time import ctime
 
+#Inizialize Flask
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'travel_around'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+mongo = PyMongo(app)
+
+#~~~~~~~~~~~~~~~~~~Testing if mongoDB database is imported~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+cities = mongo.db.cities.find({'city_name': 'berlin'})
+for item in cities:
+    print (item)
+    print()
+ 
 #~~~~~~~~~~~~~~~~~~Testing if the json file is imported~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 with open('data/countries.json') as json_file:
     json_file = json.loads(json_file.read())
     for name in json_file:
-        print (name['name'])
-        
+        if name['name'].startswith("Au"):
+            print (name['name'])
+            print ('')
 
+
+#~~~~~~~~~~~~~~~~~~Testing if the date is correctly setted~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+time = ctime()
+print ("actual time is: " + time)
+print('')
+
+#~~~~~~~~~~~~~~~~~~Testing the links ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
