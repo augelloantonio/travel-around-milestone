aa48f4ed3a37457d87660ed2afd54cc163e910ed
Ubuntu <ubuntu@ip-172-31-89-208.ec2.internal>
2019-08-07 11:35:58 +0000

    added test to check if region.json is imported

--- test.py
+++ test.py
@@ -1,32 +1,45 @@
 import os
 import json
+from app import app
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
 from time import ctime
 import unittest
 
+
 #Inizialize Flask
-app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'travel_around'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
 
+
 #~~~~~~~~~~~~~~~~~~Testing if mongoDB database is imported~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
 cities = mongo.db.cities.find({'city_name': 'berlin'})
 for item in cities:
     print (item)
     print()
+    
  
-#~~~~~~~~~~~~~~~~~~Testing if the json file is imported~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#~~~~~~~~~~~~~~~~~~Testing if the json files are imported~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
-with open('data/countries.json') as json_file:
-    json_file = json.loads(json_file.read())
-    for name in json_file:
+
+with open('data/countries.json') as json_file_country:
+    json_file_country = json.loads(json_file_country.read())
+    for name in json_file_country:
         if name['name'].startswith("Au"):
             print (name['name'])
             print ('')
+
+#testing region directory
+with open('data/region.json') as json_file_region:
+    json_file_region = json.loads(json_file_region.read())
+    for region in json_file_region:
+        if region['region_name']:
+            print(region['region_name'])
+
 
 
 #~~~~~~~~~~~~~~~~~~Testing if the date is correctly setted~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -39,7 +52,7 @@ print('')
 
 
 #~~~~~~~~~~~~~~~~~~Testing the links ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-
+"""
 class testing_links(unittest.TestCase):
     
     def setUp(self):
@@ -53,6 +66,6 @@ class testing_links(unittest.TestCase):
         response = self.mongo.get('/add_city')
         self.assertEqual(response.status_code, 200)
         
-        
+"""
 if __name__ == '__main__':
     unittest.main()
