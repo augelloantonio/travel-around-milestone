8a7fdd27d2f4d2698fe2bbe98031bb34059f69fe
Ubuntu <ubuntu@ip-172-31-89-208.ec2.internal>
2019-08-18 22:33:00 +0000

    testing performed better

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import os
 import json
 from app import app
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
 from time import ctime, strftime
@@ -17,44 +17,47 @@ mongo = PyMongo(app)
 
 #~~~~~~~~~~~~~~~~~~Testing if mongoDB database is imported~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
-cities = mongo.db.cities.find({'city_name': 'berlin'})
-for item in cities:
-    print (item)
-    print()
+class test_cities_collection(unittest.TestCase):
+    def test_cities_collection(self):
+        cities = mongo.db.cities.find({'city_name': 'berlin'})
+        for item in cities:
+            print (item)
+            print('')
+        print('test collection passed')
     
  
 #~~~~~~~~~~~~~~~~~~Testing if the json files are imported~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
-
-with open('data/countries.json') as json_file_country:
-    json_file_country = json.loads(json_file_country.read())
-    for name in json_file_country:
-        if name['name'].startswith("Au"):
-            print (name['name'])
-            print ('')
+class test_country_collection(unittest.TestCase):
+    def test_country_collection(self):
+        with open('data/countries.json') as json_file_country:
+            json_file_country = json.loads(json_file_country.read())
+            for name in json_file_country:
+                if name['name'].startswith("Au"):
+                    print (name['name'])
+                    print('')
+        print ('Test region collection passed')
 
 #testing region directory
-with open('data/region.json') as json_file_region:
-    json_file_region = json.loads(json_file_region.read())
-    for region in json_file_region:
-        if region['region_name']:
-            print(region['region_name'])
-            print('')
+class test_region_collection(unittest.TestCase):
+    def test_region_collection(self):
+        with open('data/region.json') as json_file_region:
+            json_file_region = json.loads(json_file_region.read())
+            for region in json_file_region:
+                if region['region_name']:
+                    print(region['region_name'])
+                    print('')
+        print('Test region collection passed')
         
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~Testing if time is printed~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-print(strftime('Time is: ' + '%d' + "/" + '%m' + "/"+ '%Y'))
-print('')
-
-
-#~~~~~~~~~~~~~~~~~~Testing if the date is correctly setted~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-
 class test_timen(unittest.TestCase):
     def test_time(self):
-        time = ctime()
-        print ("actual time is: " + time)
+        print(strftime('Time is: ' + '%d' + "/" + '%m' + "/"+ '%Y'))
         print('')
-
+    print('Time test passed')
+    
+    
 #~~~~~~~~~~~~~~~~~~Testing login form~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 class testing_login(unittest.TestCase):
     def test_login(self):
@@ -70,20 +73,23 @@ class testing_login(unittest.TestCase):
     
 
 #~~~~~~~~~~~~~~~~~~Testing the links ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-"""
+
 class testing_links(unittest.TestCase):
+    
+
     
     def setUp(self):
         self.mongo = app.test_client()
 
     def test_home(self):
         page = self.mongo.get('/')
-        self.assertEqual(page.status_code, 202)
-
-    def test_add_city(self):
-        response = self.mongo.get('/add_city')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(page.status_code, 200)
+    
+    def test_login_page(self):
+        page = self.mongo.get('/login_page')
+        self.assertEqual(page.status_code, 200)
         
-"""
+    print('Tests passed')
+
 if __name__ == '__main__':
     unittest.main()
